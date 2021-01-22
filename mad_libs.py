d2a4b2accd465da328c0157840762fc4ed0b73b1
onias17 <oniasnephiisrael@gmail.com>
2021-01-22 13:26:49 -0800

    madlibs

---
+++ mad_libs.py
@@ -0,0 +1,9 @@
+# MAD LIBS
+
+direction = input("enter southern or northern: ")
+tribe = input("enter your tribe: ")
+name = input("enter your name: ")
+
+print("I'm from the " + direction + " kingdom of Israel")
+print("I'm from the tribe of " + tribe)
+print("My name is " + name)
